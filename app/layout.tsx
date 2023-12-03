0121a7608f839f2fa401cd2acbaa6d6141f9870f
Yutong Zhu <Yutong2022@gmail.com>
2023-12-03 16:57:53 -0500

    lowercase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { AOSInit } from './components/AOS'
+import { AOSInit } from './components/aos'
 const inter = Inter({ subsets: ['latin'] })
 import Nav from './components/Nav';
 import { Suspense } from 'react'
